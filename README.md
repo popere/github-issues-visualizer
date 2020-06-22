2e7af0feef04a24fd1b97d4efd734e6b1b434e43
popere <popere@gmail.com>
2020-06-22 19:07:01 +0200

    delete initial html content

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IrontecProject
+# Github Issues Visualizer
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.9.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'irontec-project'`, () => {
+  it(`should have as title 'github-issues-visualizer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('irontec-project');
+    expect(app.title).toEqual('github-issues-visualizer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('irontec-project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('github-issues-visualizer app is running!');
   });
 });
